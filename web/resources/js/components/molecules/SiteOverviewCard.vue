acd1f6c4c2e8a7f2aa61d9f461ea4ce283b0f1ce
D_nishimura <11kakeru30@gmail.com>
2021-02-11 19:49:48 +0900

    サイト概要（生体認証）の完成

--- web/resources/js/components/molecules/SiteOverviewCard.vue
+++ web/resources/js/components/molecules/SiteOverviewCard.vue
@@ -1,6 +1,32 @@
 <template>
-  <v-card height="100">
+  <v-card flat>
+    <v-row>
+      <v-col cols="6" class="pa-0 my-auto text-center">
+        <v-col cols="12">
+          <v-icon size="80"><slot name="icon-face"></slot></v-icon>
+          <v-icon size="40" class="primary--text">mdi-forward</v-icon>
+          <v-icon size="80">mdi-database-search</v-icon>
+        </v-col>
 
+        <v-col cols="12">
+          <v-icon size="80"><slot name="icon-fing"></slot></v-icon>
+          <v-icon size="40" class="primary--text">mdi-forward</v-icon>
+          <v-icon size="80">mdi-database-search</v-icon>
+        </v-col>
+      </v-col>
+
+      <v-col cols="6">
+        <v-card-title class="pa-0">
+          <h4 class="text-h4">
+            <slot name="title"></slot>
+          </h4>
+        </v-card-title>
+        <v-card-text class="pa-0 mt-4">
+          <slot name="text"></slot>
+        </v-card-text>
+      </v-col>
+
+    </v-row>
   </v-card>
 </template>
 
