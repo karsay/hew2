d066b5fd139e67ff6ac7ea8633c7c3a38d0cc66f
D_nishimura <11kakeru30@gmail.com>
2021-02-12 01:13:49 +0900

    ページネーションによる動的ルーティングを追加

---
+++ web/resources/js/pages/ProductListsPage.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+    <v-img src="" alt="" srcset="" height="240" class="primary_dark" />
+
+    <v-card class="pb-12 card card__position">
+
+      <v-container class="py-10 px-6">
+        <v-card-title class="pl-0">
+          新着アイテム
+        </v-card-title>
+
+        <v-pagination
+          v-model="pageNum"
+          :length="6"
+          @input="pagenationHandle"
+        >
+        </v-pagination>
+      </v-container>
+
+    </v-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        pageNum: 1
+      }
+    },
+    methods: {
+      pagenationHandle() {
+        this.$router.push(`/productList/${this.pageNum}`)
+      }
+    },
+  }
+</script>
+
+<style scoped>
+.card {
+  border-radius: 16px !important;
+}
+
+.card__position {
+  position: relative;
+  top: -16px;
+}
+</style>
