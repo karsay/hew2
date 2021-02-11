9716391a528b81f3380499bc25de71b5d4475c64
D_nishimura <11kakeru30@gmail.com>
2021-02-11 17:12:00 +0900

    Relief_Marketの概要を追加

--- web/resources/js/pages/TopPage.vue
+++ web/resources/js/pages/TopPage.vue
@@ -4,7 +4,7 @@
 
     <v-card class="card">
 
-      <v-container class="pt-10 pr-6 pl-6 pb-10">
+      <v-container class="py-10 px-6">
         <v-card-title class="pl-0">
           新着アイテム
         </v-card-title>
@@ -27,20 +27,23 @@
 
       <v-divider></v-divider>
 
-      <v-container class="pt-10 pr-6 pl-6 pb-10">
+      <v-container class="py-10 px-6">
         <v-card-title class="pl-0">
           ピックアップカテゴリ
         </v-card-title>
 
-        <v-card-text class="pa-0">
-          subtitle
-        </v-card-text>
         <v-row
-          class="pt-0"
+          v-for="pickUpProduct in pickUpProducts"
+          :key="pickUpProduct.id"
+          class="pt-0 mb-6"
         >
+        <v-card-text class="pb-1 pl-3">
+          {{ pickUpProduct.category }}
+        </v-card-text>
           <v-col
             v-for="value in val"
             :key="value"
+            class="py-0"
           >
             <product-card
               :product-image="productImage"
@@ -55,18 +58,25 @@
 
       <v-divider></v-divider>
 
-      <v-container class="pt-10 pr-6 pl-6 pb-10">
-        <v-row
-          class="pt-0"
-        >
-          <v-col
-            v-for="value in val"
-            :key="value"
+      <site-overview>
+        <template #overview-title>Relief Marketとは？</template>
+        <template #overview-subtitle>IoTデバイス</template>
+        <template #iot-overview>
+          <p
+            v-for="iotOverview in siteOverview.iotOverview"
+            :key="iotOverview"
+            class="ma-0 text-body-1 font-weight-regular"
           >
-          </v-col>
-        </v-row>
-      
+            {{ iotOverview }}
+          </p>
+        </template>
+        <template #button-text>登録する</template>
+      </site-overview>
+
+      <v-container class="pt-8 px-6">
+        <site-overview-card></site-overview-card>
       </v-container>
+
     </v-card>
   </div>
 
@@ -74,17 +84,80 @@
 
 <script>
 import ProductCard from './../components/molecules/ProductCard'
+import SiteOverview from './../components/molecules/SiteOverview'
+import SiteOverviewCard from './../components/molecules/SiteOverviewCard'
+
+/*
+  # apiから受け取る
+    - 新着アイテムの情報
+    - ピックアップカテゴリの名前、アイテムのデータ
+*/
 
 export default {
   components: {
     ProductCard,
+    SiteOverview,
+    SiteOverviewCard
   },
   data() {
     return {
+      sectionTitle: {
+        newProduct: "新着アイテム",
+        pickUpCategory: "ピックアップカテゴリ",
+        siteOverview: "Relief Marketとは？"
+      },
       val: new Array(5),
       productImage: {
         path: '',
         name: 'productName'
+      },
+      pickUpProducts: [
+        {
+          id: 1,
+          category: 'カテゴリ1',
+          products: [
+            {
+              imagePath: '',
+              imageName: '',
+              name: 'productName',
+              price: '1,000',
+              heart: 33
+            }
+          ]
+        },
+        {
+          id: 2,
+          category: 'カテゴリ2',
+          products: [
+            {
+              imagePath: '',
+              imageName: '',
+              name: 'productName',
+              price: '1,000',
+              heart: 33
+            }
+          ]
+        },
+        {
+          id: 3,
+          category: 'カテゴリ3',
+          products: [
+            {
+              imagePath: '',
+              imageName: '',
+              name: 'productName',
+              price: '1,000',
+              heart: 33
+            }
+          ]
+        },
+      ],
+      siteOverview: {
+        iotOverview: [
+          "Relief Marketでは会員登録、ログイン、商品の出品、購入する際にユーザー本人であるかどうかを顔認証、指紋情報二つの生体情報を用いて登録、確認を行います。",
+          "認証情報を取得する際、Relief Marketではユーザーに指紋認証、顔認証を可能とするIoTデバイスを利用していただき、サービスを使えるようにします。",
+          "",
+        ]
       }
     }
   }
