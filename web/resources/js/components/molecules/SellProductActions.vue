f558c32a09317be702c759ea02cbae10f6dc780a
D_nishimura <11kakeru30@gmail.com>
2021-02-14 17:53:39 +0900

    出品のプレビューとキャンセルをコンポーネント化

---
+++ web/resources/js/components/molecules/SellProductActions.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-col class="d-flex px-0">
+    <v-btn
+      width="150"
+      color="primary"
+      class="mr-6"
+      to="preview"
+    >
+      出品プレビュー
+    </v-btn>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      width="150"
+      color="primary"
+      text
+    >
+      キャンセル
+    </v-btn>
+  </v-col>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
