a2cc18a19ff1a86601a3b8985fcda56903ba22c2
D_nishimura <11kakeru30@gmail.com>
2021-02-20 22:05:35 +0900

    input,textareaのvalueをvuexへ追加する処理を追加

--- web/resources/js/components/container/TheProductInputForm.vue
+++ web/resources/js/components/container/TheProductInputForm.vue
@@ -1,11 +1,14 @@
 <template>
   <ProductInputForm
     :inputItems="inputItems"
+    :textareaItem="textareaItem"
+    @set-input-items="setInputItems"
   />
 </template>
 
 <script>
 import ProductInputForm from './../organisms/ProductInputForm'
+import { mapMutations } from 'vuex'
 
 export default {
   components: { ProductInputForm },
@@ -13,14 +16,28 @@ export default {
     return {
       inputItems: [
         {
+          prop: 'name',
           label: "商品名",
+          type: 'text',
+          value: ""
         },
         {
+          prop: 'price',
           label: "価格",
+          type: 'number',
+          value: ""
         }
-      ]
+      ],
+      textareaItem: {
+        label: "商品説明",
+        prop: 'detail',
+        value: ""
+      }
     }
-  }
+  },
+  methods: {
+    ...mapMutations('sellProduct', ['setInputItems']),
+  },
 }
 </script>
 
