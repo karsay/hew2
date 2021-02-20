e025d0db22eb60b3af7665c817ba8cae788a979c
D_nishimura <11kakeru30@gmail.com>
2021-02-21 03:35:57 +0900

    商品詳細の一つずつをコンポーネント化

---
+++ web/resources/js/components/molecules/ProductDetailItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-card-subtitle class="pa-0">
+      <p class="ma-0">{{ label }}</p>
+    </v-card-subtitle>
+    <v-card-text class="pb-2 px-0">
+      <p class="ma-0 text-h6 card__text">{{ productItem }}</p>
+    </v-card-text>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    productItem: String
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.card__text {
+  border-bottom: solid 1px rgba($color: #000, $alpha: .37);
+}
+</style>
