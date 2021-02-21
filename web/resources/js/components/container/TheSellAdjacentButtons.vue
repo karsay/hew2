330d09b15336f8a1587eef40014278166030c7f8
D_nishimura <11kakeru30@gmail.com>
2021-02-21 17:40:13 +0900

    出品ページの隣接するボタンのロジックを分離

---
+++ web/resources/js/components/container/TheSellAdjacentButtons.vue
@@ -0,0 +1,30 @@
+<template>
+  <AdjacentButtons
+    :buttons="buttons"
+    @on-click="toPreview"
+  />
+</template>
+
+<script>
+import AdjacentButtons from '../molecules/AdjacentButtons'
+
+export default {
+  components: { AdjacentButtons },
+  data() {
+    return {
+      buttons: {
+        text: "出品プレビュー"
+      }
+    }
+  },
+  methods: {
+    toPreview() {
+      this.$router.push('preview')
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
