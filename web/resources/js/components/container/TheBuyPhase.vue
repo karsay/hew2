aafb7d171bfbd4dccc652e5594bc365fd78e49d3
D_nishimura <11kakeru30@gmail.com>
2021-02-23 15:18:29 +0900

    paramsの取得 radioボタンのロジックを定義

---
+++ web/resources/js/components/container/TheBuyPhase.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+  <BuyPhase
+    :details="details"
+    :radio="radio"
+    @is-radio="isRadio"
+  />
+  </div>
+</template>
+
+<script>
+import BuyPhase from '../organisms/BuyPhase'
+
+export default {
+  components: { BuyPhase },
+  data() {
+    return {
+      radio: true
+    }
+  },
+  methods: {
+    isRadio() {
+      this.radio = !this.radio
+    },
+  },
+  computed: {
+    details() {
+      return this.$route.params.details
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
