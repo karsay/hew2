f9275ae934ea5c651f0950e8154946ecc7af9f58
karsay <ryu47seinou@gmail.com>
2021-02-25 21:49:22 +0900

    ログイン画面エラーハンドリング追加

--- web/resources/js/pages/Login.vue
+++ web/resources/js/pages/Login.vue
@@ -3,7 +3,7 @@
     <v-card
       width="100%"
       height="100%"
-      class="mx-auto mt-16"
+      class="mx-auto"
       color="#F0F3F5"
       elevation-10
       max-height="680"
@@ -13,8 +13,6 @@
         <v-col align="center">
           <iframe
             :src="streamUrl"
-            width="400"
-            height="600"
             frameborder="0"
             style="border: 0"
             allowfullscreen
@@ -29,8 +27,12 @@
                 <img src="/assets/img/user_icon.png" class="ma-5" width="150px" height="150px" />
               </v-col>
               <v-col>
-                <h2 v-if="flag1 == false" class="red--text hoge">カメラを顔に向けて画面を<br>ダブルクリックしてください</h2>
-                <h2 v-else class="green--text hoge">ようこそ【{{userName}}】さん</h2>
+                <h2 v-if="flag1 == false" class="red--text">カメラを顔に向けて画面を<br>ダブルクリックしてください</h2>
+                <span v-else>
+                  <h2 v-if="errorName" class="red--text">登録された顔と一致しませんでした...<br>もう一度やり直してください</h2>
+                  <h2 v-else class="green--text">ようこそ【{{userName}}】さん</h2>
+                </span>
+
               </v-col>
             </v-row>
             <v-row align="center">
@@ -38,10 +40,10 @@
                 <img src="/assets/img/finger_icon.png" class="ma-5" width="150px" height="150px">
               </v-col>
               <v-col justifu="center">
-                <h2 v-if="flag2 == false" class="red--text hoge">指紋を認証してください</h2>
+                <h2 v-if="flag2 == false" class="red--text">指紋を認証してください</h2>
                 <span v-else>
-                  <h2 v-if="this.complateFlag.data == 'true'" class="green--text hoge">認証成功！！</h2>
-                  <h2 v-else class="red--text hoge">認証失敗・・・<br>もう一度やり直して下さい</h2>
+                  <h2 v-if="this.complateFlag.data == 'true'" class="green--text">認証成功！！</h2>
+                  <h2 v-else class="red--text">認証失敗...<br>もう一度やり直して下さい</h2>
                 </span>
               </v-col>
             </v-row>
@@ -73,6 +75,7 @@ export default {
       // raspAddress:"192.168.43.108",
       // hostAddress:"192.168.43.6",
       userName:"",
+      errorName:false,
 
       flag1:false,
       flag2:false,
@@ -96,23 +99,25 @@ export default {
   methods: {
 
     async click() {
-      await axios.post(`http://${this.raspAddress}:5000/shutter`)
+      await axios.post(`http://${this.raspAddress}:5000/shutter`);
     },
 
     async login () {
       // authストアのloginアクションを呼び出す
-      await this.$store.dispatch('auth/login', this.loginForm)
+      await this.$store.dispatch('auth/login', this.loginForm);
 
       // トップページに移動する
-      this.$router.push('/')
+      this.$router.push('/');
     },
 
     // 認証開始
     async authentication() {
-      this.hoge = null
       this.streamUrl = `http://${this.raspAddress}:5000/stream`
       // 初期化
-      this.loginForm.userId = null
+      this.loginForm.userId = null;
+      this.errorName = false;
+      this.flag1 = false;
+      this.flag2 = false;
 
       // ここのipアドレスをラズパイのアドレスに書き換えること
       const response = await axios.post(`http://${this.raspAddress}:5000`,{
@@ -120,28 +125,30 @@ export default {
         userId:null,
       });
 
-      this.loginForm.userId = response.data[0];
-      this.loginForm.fingerPass = response.data[1];
-
-      var res = await axios.post(`/api/getlogininfo`,{'userId':this.loginForm.userId});
-      this.userName = res.data;
-
       this.flag1 = true;
+      if(response.data[0] == "error") {
+        this.errorName = true;
+      } else {
+        this.loginForm.userId = response.data[0];
+        this.loginForm.fingerPass = response.data[1];
 
-      // 指紋認証
-      // ここのipアドレスをラズパイのアドレスに書き換えること
-      this.complateFlag = await axios.post(`http://${this.raspAddress}:5000/fingerprint`,{
-        userId:this.loginForm.userId,
-        fingerPass:this.loginForm.fingerPass,
-        fingerFlag:2
-      });
-      if (this.complateFlag.data == "true"){
-        this.isDisabled = false
+        var res = await axios.post(`/api/getlogininfo`,{'userId':this.loginForm.userId});
+        this.userName = res.data;
+
+        // 指紋認証
+        // ここのipアドレスをラズパイのアドレスに書き換えること
+        this.complateFlag = await axios.post(`http://${this.raspAddress}:5000/fingerprint`,{
+          userId:this.loginForm.userId,
+          fingerPass:this.loginForm.fingerPass,
+          fingerFlag:2
+        });
+        if (this.complateFlag.data == "true"){
+          this.isDisabled = false
+        }
+        this.flag2 = true;
+        this.streamUrl = "";
       }
-
-      this.flag2 = true;
-      this.streamUrl = ""
-    },
+    }
   },
 }
 </script>
@@ -151,16 +158,15 @@ export default {
   text-decoration: none;
 }
 .area {
-  width: 100vw;
-  height: 100%;
+  width: 100%;
+  height: 690px;
   position: absolute;
 }
 
 iframe {
   border: none;
   width: 100%;
-  height: 100%;
-  min-height: 650px;
+  height: 680px;
   padding: 0;
   margin: 0;
 }
@@ -168,6 +174,5 @@ iframe {
   object-fit: cover;
   width: 100%;
   height: 100%;
-  /* min-height: 650px; */
 }
 </style>
