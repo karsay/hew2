e0c3b1bff7d5df95970f5a38ccca0ab5bef36a99
D_nishimura <11kakeru30@gmail.com>
2021-02-27 20:23:41 +0900

    loadingCardのデータを定義

---
+++ web/resources/js/components/container/TheLoadingProductCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <LoadingProductCard
+    :path="path"
+    :name="name"
+    :price="price"
+  />
+</template>
+
+<script>
+import LoadingProductCard from '../molecules/LoadingProductCard'
+
+export default {
+  components: { LoadingProductCard },
+  data() {
+    return {
+      path: '',
+      name: "Relief Market",
+      price: "loading..."
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
