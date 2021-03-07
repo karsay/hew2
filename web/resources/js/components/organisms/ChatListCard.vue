f025fb48e1bc6482f410abc7382d2af3221170cd
D_nishimura <11kakeru30@gmail.com>
2021-03-08 02:02:02 +0900

    テストで4つ商品を並べた

---
+++ web/resources/js/components/organisms/ChatListCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card elevation="1" class="card-size">
+    <v-toolbar color="primary_dark" class="title ma-0" dark>
+      <v-toolbar-title class="ma-0">
+        取引相手一覧
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <div class="overflow">
+      <ChatCard />
+      <ChatCard />
+      <ChatCard />
+      <ChatCard />
+    </div>
+
+  </v-card>
+</template>
+
+<script>
+import ChatCard from '../molecules/ChatCard'
+
+export default {
+  components: {
+    ChatCard,
+  }
+}
+</script>
+
+<style scoped>
+
+.card-size {
+  height: 100%;
+}
+.overflow {
+  height: calc(80vh - 88px);
+  overflow: scroll;
+}
+
+</style>
