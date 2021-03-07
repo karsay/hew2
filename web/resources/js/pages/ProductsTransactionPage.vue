a7d3925a3bac83e3e27fe973dd31bd6edca032d8
D_nishimura <11kakeru30@gmail.com>
2021-03-08 02:00:46 +0900

    chatlistcardを追加

--- web/resources/js/pages/ProductsTransactionPage.vue
+++ web/resources/js/pages/ProductsTransactionPage.vue
@@ -1,17 +1,24 @@
 <template>
-  <v-container>
+  <v-container class="container d-flex justify-center align-center">
     <ChatView />
+    <ChatListCard />
   </v-container>
 </template>
 
 <script>
+import ChatListCard from '../components/organisms/ChatListCard.vue'
 import ChatView from '../components/organisms/ChatView'
 
 export default {
-  components: { ChatView },
+  components: {
+    ChatView,
+    ChatListCard
+  },
 }
 </script>
 
 <style scoped>
-
+.container {
+  height: 80vh;
+}
 </style>
