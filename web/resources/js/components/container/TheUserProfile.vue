47d40f98645ba80698ab03550226c7cb41d8ecdf
D_nishimura <11kakeru30@gmail.com>
2021-03-08 22:30:21 +0900

    userProfileのカードのロジックとデザインを分離

---
+++ web/resources/js/components/container/TheUserProfile.vue
@@ -0,0 +1,24 @@
+<template>
+  <UserProfile
+    :userName="username"
+    :usersBirthday="usersBirthday"
+  />
+</template>
+
+<script>
+import UserProfile from '../organisms/UserProfile'
+import { mapGetters } from 'vuex'
+
+export default {
+  components: {
+    UserProfile
+  },
+  computed: {
+    ...mapGetters('auth', ['username', 'usersBirthday'])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
