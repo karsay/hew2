9213742d2b97590946c06868d5310f4b933ab1b5
D_nishimura <11kakeru30@gmail.com>
2021-03-08 23:11:16 +0900

    運営からのお知らせの部分を作成

---
+++ web/resources/js/components/container/TheNoticeFromOperation.vue
@@ -0,0 +1,18 @@
+<template>
+  <NoticeFromOperation
+  />
+</template>
+
+<script>
+import NoticeFromOperation from '../organisms/NoticeFromOperation'
+
+export default {
+  components: {
+    NoticeFromOperation,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ web/resources/js/components/organisms/NoticeFromOperation.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card>
+    <div class="pa-6">
+      <v-card-title class="pa-0 primary--text">
+        運営からのお知らせ
+      </v-card-title>
+
+      <v-divider class="mt-6 mb-4"></v-divider>
+
+      <div>
+        <v-card>
+          <v-card-subtitle class="pb-0 primary_light--text">
+            2020-03-01
+          </v-card-subtitle>
+          <v-card-title class="text-h6 pt-0">
+            Relief Marketへようこそ！
+          </v-card-title>
+        </v-card>
+      </div>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
