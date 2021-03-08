c8d2cb51e6030604b438d7c3ba46e7654e6329fa
D_nishimura <11kakeru30@gmail.com>
2021-03-08 22:30:41 +0900

    theuserprofileをimport

--- web/resources/js/pages/UserProfilePage.vue
+++ web/resources/js/pages/UserProfilePage.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>
-    user profile page
-  </div>
+  <v-container class="pt-0 d-flex justify-center">
+    <v-sheet width="720" color="#fafafa" class="px-6 mt-10">
+      <div class="text-h4">プロフィール</div>
+      <v-row class="pt-6">
+        <v-col cols="6">
+          <TheUserProfile />
+        </v-col>
+
+        <v-col cols="6">
+          <TheUserProfile />
+        </v-col>
+      </v-row>
+      <v-divider class="my-10"></v-divider>
+      <div class="text-h4">購入した商品</div>
+      <v-divider></v-divider>
+      <div class="text-h4">出品した商品</div>
+    </v-sheet>
+  </v-container>
 </template>
 
 <script>
+import TheUserProfile from '../components/container/TheUserProfile'
+
+
 export default {
+  components: { TheUserProfile },
   
 }
 </script>
